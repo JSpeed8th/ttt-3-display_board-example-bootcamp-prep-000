d3b45e17102eaaa16077fc6e2807101710cecfa6
JSpeed8th <jspeed8th@gmail.com>
2018-02-22 04:10:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@
 
 
 def display_board
-  puts "    |    |    "
+  puts "   |   |  "
   puts "-----------"
-  puts "    |    |    |"
+  puts "   |   |   "
   puts "-----------"
-  puts "    |    |    |"
+  puts "   |   |   "
 end
